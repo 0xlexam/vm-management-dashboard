5ca11cc9b8ef64b80e86e33831405c5d015c64f5
0xlexam <163334270+0xlexam@users.noreply.github.com>
2024-09-12 17:04:56 +0300

    cleaning

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
 import os
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -33,4 +33,5 @@ def delete_vm(vm_id):
     return jsonify({"message": "VM deleted", "vm_id": vm_id})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5000))
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
