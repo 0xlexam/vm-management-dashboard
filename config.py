f8a0630a19a1377c5df437b0042ab7eda86eb29d
0xlexam <163334270+0xlexam@users.noreply.github.com>
2024-12-05 15:12:29 +0200

    update variables

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY", "Your_Default_API_Key")
+# Retrieve the API key from environment variables or use a default
+SERVER_API_KEY = os.getenv("API_KEY", "Your_Default_API_Key")
 
-DATABASE = {
+# Database configuration details, with clearer naming
+DATABASE_CONFIG = {
     'NAME': os.getenv("DB_NAME", "default_db_name"),
-    'USER': os.getenv("DB_USER", "default_user"),
+    'USERNAME': os.getenv("DB_USER", "default_user"),
     'PASSWORD': os.getenv("DB_PASSWORD", "default_password"),
-    'HOST': os.getenv("DB_HOST", "localhost"),
-    'PORT': int(os.getenv("DB_PORT", 5432)),
+    'HOST_ADDRESS': os.getenv("DB_HOST", "localhost"),
+    'PORT_NUMBER': int(os.getenv("DB_PORT", 5432)),
 }
 
-DEBUG_MODE = os.getenv("DEBUG_MODE", "False").lower() in ('true', '1', 't')
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+# Toggle for enabling or disabling debug mode based on environment variable
+IS_DEBUG_MODE_ENABLED = os.getenv("DEBUG_MODE", "False").lower() in ('true', '1', 't')
+
+# Application log level setting
+APPLICATION_LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
